67255cc8d8100fe44cab134f2f493639e98a9ab8
lucas <lucas.gangnant@epitech.com>
2024-12-14 18:47:44 +0800

    Feature: Implemented basic hook system

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -7,8 +7,39 @@ extends CharacterBody3D
 @export var state_machine: StateMachine
 
 var weapon: WeaponController
+@onready var hook_cast = $FirstPersonCamera/Camera3D/HookCast3D
+@onready var bonker = $FirstPersonCamera/HeadBonker
+
+var hooking = false
+var hookpoint = Vector3()
+var hookpoint_get = false
 
 var _direction: Vector3
+
+func hook():
+	if Input.is_action_just_pressed("spell"):
+		print("Hook Pressed")
+		if hook_cast.is_colliding():
+			print("Hook Collide")
+			if not hooking:
+				print("Hook NOT Collide")
+				hooking = true
+	if hooking:
+		velocity.y = 0
+		if not hookpoint_get:
+			hookpoint = hook_cast.get_collision_point() + Vector3(0, 2.25, 0)
+			hookpoint_get = true
+		if hookpoint.distance_to(transform.origin) > 1:
+			if hookpoint_get:
+				transform.origin = lerp(transform.origin, hookpoint, 0.05)
+		else:
+			hooking = false
+			hookpoint_get = false
+	if bonker.is_colliding():
+		hooking = false
+		hookpoint = null
+		hookpoint_get = false
+		global_translate(Vector3(0, -3, 0))
 
 func _ready() -> void:
 	shapecast.add_exception(self)
@@ -21,10 +52,11 @@ func _ready() -> void:
 
 	Globals.player = self
 
+func _process(delta: float):
+	hook()
 
 func update_gravity(delta: float) -> void:
 	velocity += get_gravity() * delta
-
 
 func update_input(speed: float, acceleration: float, deceleration: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
@@ -36,7 +68,6 @@ func update_input(speed: float, acceleration: float, deceleration: float) -> voi
 		velocity.x = move_toward(velocity.x, 0, deceleration)
 		velocity.z = move_toward(velocity.z, 0, deceleration)
 
-
 func update_speed(speed: float, acceleration: float, deceleration: float) -> void:
 	if _direction:
 		velocity.x = lerp(velocity.x, _direction.x * speed, acceleration)
@@ -44,7 +75,6 @@ func update_speed(speed: float, acceleration: float, deceleration: float) -> voi
 	else:
 		velocity.x = move_toward(velocity.x, 0, deceleration)
 		velocity.z = move_toward(velocity.z, 0, deceleration)
-
 
 func update_velocity() -> void:
 	move_and_slide()
